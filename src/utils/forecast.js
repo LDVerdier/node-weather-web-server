46131a6a24a9b70eb696ae5f09c53172a34f32dd
laurent <ldverdier@gmail.com>
2021-08-08 12:24:16 +0200

    updated data provided by the app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.humidity + ' humidity degree.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.humidity + ' humidity percentage. The pressure is ' + body.current.pressure + ' hectopascals.')
         }
     })
 }
